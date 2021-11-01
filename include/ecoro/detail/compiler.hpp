66a2d6ea6b471490c4ad7f5e404da57f83093d36
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2021-11-01 21:53:01 +0100

    msvc: Add compiler detection
    This define will be useful for later MSVC support

---
+++ include/ecoro/detail/compiler.hpp
@@ -0,0 +1,13 @@
+// Copyright 2021 - present, Mikhail Svetkin
+// All rights reserved.
+//
+// For the license information refer to LICENSE
+
+#ifndef ECORO_DETAIL_COMPILER
+#define ECORO_DETAIL_COMPILER
+
+#if _MSC_VER
+#  define ECORO_COMPILER_MSVC _MSC_VER
+#endif
+
+#endif  // ECORO_DETAIL_COMPILER
