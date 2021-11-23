aea81cd1b6c9f7f3a5d1fc5387c64c2c899638e5
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2021-11-23 20:38:38 +0100

    when_first: Initial implementation

---
+++ include/ecoro/when_first.hpp
@@ -0,0 +1,32 @@
+// Copyright 2021 - present, Mikhail Svetkin
+// All rights reserved.
+//
+// For the license information refer to LICENSE
+
+#ifndef ECORO_WHEN_FIRST_HPP
+#define ECORO_WHEN_FIRST_HPP
+
+#include "ecoro/detail/traits.hpp"
+#include "ecoro/task.hpp"
+#include "ecoro/when_any.hpp"
+
+#include <optional>
+
+namespace ecoro {
+
+template<typename Awaitable, typename Trigger>
+[[nodiscard]] task<std::optional<awaitable_return_t<Awaitable>>> when_first(
+    Awaitable awaitable, Trigger trigger) {
+  auto &&[index, awaitables] =
+      co_await when_any(std::move(awaitable), std::move(trigger));
+
+  if (index == 0) {
+    co_return std::get<0>(std::move(awaitables)).result();
+  }
+
+  co_return std::nullopt;
+}
+
+}  // namespace ecoro
+
+#endif  // ECORO_WHEN_FIRST_HPP
